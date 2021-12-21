74f7ea3efd4775e9043fb2d63435a47333f726de
hodgerpodger <>
2021-12-21 12:43:47 -0800

    Fixed osmo swap bug  Please enter the commit message for your changes. Lines starting

--- src/osmo/handle_swap.py
+++ src/osmo/handle_swap.py
@@ -6,11 +6,12 @@ from common.make_tx import make_swap_tx
 def handle_swap(exporter, txinfo, message, transfers):
     transfers_in, transfers_out = transfers
 
-    if len(transfers) == 1 and len(transfers_out) == 1:
+    if len(transfers_in) == 1 and len(transfers_out) == 1:
         sent_amount, sent_currency = transfers_out[0]
         received_amount, received_currency = transfers_in[0]
 
-        make_swap_tx(txinfo, sent_amount, sent_currency, received_amount, received_currency)
+        row = make_swap_tx(txinfo, sent_amount, sent_currency, received_amount, received_currency)
+        exporter.ingest_row(row)
         return
 
     handle_unknown_detect_transfers(exporter, txinfo, transfers)
