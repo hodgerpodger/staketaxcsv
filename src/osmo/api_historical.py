7879b75d242d796ee6959e7d9e0b4d9dcb2966dd
DTing <dennisting@gmail.com>
2022-01-04 00:47:13 -0800

    Simplify osmo api functions (#13)

--- src/osmo/api_historical.py
+++ src/osmo/api_historical.py
@@ -5,17 +5,16 @@ from osmo.api_util import _query_get
 OSMO_HISTORICAL_API_NETLOC = "api-osmosis.imperator.co"
 
 
-def _query(netloc, uri_path, query_params):
-    result = _query_get(netloc, uri_path, query_params)
+def _query(uri_path, query_params):
+    result = _query_get(OSMO_HISTORICAL_API_NETLOC, uri_path, query_params)
     time.sleep(1)
     return result
 
 
 def get_symbol(ibc_address) -> str or None:
-    template = "/search/v1/symbol"
-    uri_path = template.format()
+    uri_path = "/search/v1/symbol"
     query_params = {"denom": quote(ibc_address)}
 
-    data = _query(OSMO_HISTORICAL_API_NETLOC, uri_path, query_params)
+    data = _query(uri_path, query_params)
 
     return data["symbol"] if "symbol" in data else None
--- src/osmo/api_tx.py
+++ src/osmo/api_tx.py
@@ -4,17 +4,15 @@ from osmo.api_util import _query_get
 OSMO_TX_API_NETLOC = "api-osmosis.cosmostation.io"
 
 
-def _query(netloc, uri_path, query_params):
-    result = _query_get(netloc, uri_path, query_params)
+def _query(uri_path, query_params={}):
+    result = _query_get(OSMO_TX_API_NETLOC, uri_path, query_params)
     time.sleep(1)
     return result
 
 
 def get_tx(txid):
-    template = "/v1/tx/hash/{txid}"
-    uri_path = template.format(txid=txid)
-    query_params = {}
+    uri_path = f"/v1/tx/hash/{txid}"
 
-    data = _query(OSMO_TX_API_NETLOC, uri_path, query_params)
+    data = _query(uri_path)
 
     return data["data"]
