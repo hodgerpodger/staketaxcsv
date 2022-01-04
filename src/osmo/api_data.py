0436d12d6007e5ed9c6f5cbc141c60d73e5d51b3
Dennis Ting <dennisting@gmail.com>
2022-01-03 21:14:21 -0800

    Simplify osmo api functions

--- src/osmo/api_data.py
+++ src/osmo/api_data.py
@@ -5,31 +5,28 @@ OSMO_DATA_API_NETLOC = "api-osmosis-chain.imperator.co"
 LIMIT = 25
 
 
-def _query(netloc, uri_path, query_params, sleep_seconds=1):
-    result = _query_get(netloc, uri_path, query_params)
+def _query(uri_path, query_params={}, sleep_seconds=1):
+    result = _query_get(OSMO_DATA_API_NETLOC, uri_path, query_params)
     time.sleep(sleep_seconds)
     return result
 
 
 def get_count_txs(address):
-    template = "/txs/v1/tx/count/{address}"
-    uri_path = template.format(address=address)
-    query_params = {}
+    uri_path = f"/txs/v1/tx/count/{address}"
 
-    data = _query(OSMO_DATA_API_NETLOC, uri_path, query_params)
+    data = _query(uri_path)
 
     return sum(row["count"] for row in data)
 
 
 def get_txs(address, offset=0):
-    template = "/txs/v1/tx/address/{address}"
-    uri_path = template.format(address=address)
+    uri_path = f"/txs/v1/tx/address/{address}"
     query_params = {
         "limit": LIMIT,
         "offset": offset
     }
 
-    data = _query(OSMO_DATA_API_NETLOC, uri_path, query_params)
+    data = _query(uri_path, query_params)
 
     # Extract "tx_response" (found to be common data across multiple APIs)
     return [row["tx_response"] for row in data]
@@ -37,20 +34,16 @@ def get_txs(address, offset=0):
 
 def get_lp_tokens(address):
     """ Returns list of symbols """
-    template = "/lp/v1/rewards/token/{address}"
-    uri_path = template.format(address=address)
-    query_params = {}
+    uri_path = f"/lp/v1/rewards/token/{address}"
 
-    data = _query(OSMO_DATA_API_NETLOC, uri_path, query_params)
+    data = _query(uri_path)
 
     return [kv["token"] for kv in data]
 
 
 def get_lp_rewards(address, token):
-    template = "/lp/v1/rewards/historical/{address}/{token}"
-    uri_path = template.format(address=address, token=token)
-    query_params = {}
+    uri_path = f"/lp/v1/rewards/historical/{address}/{token}"
 
-    data = _query(OSMO_DATA_API_NETLOC, uri_path, query_params)
+    data = _query(uri_path)
 
     return data
