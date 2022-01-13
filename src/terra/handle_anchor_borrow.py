b0ee5c89e414e6ac916ae1172b99d7e3342b870a
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2022-01-12 17:03:08 -0800

    terra fix fee for borrow tx (net transfer in old version was correct but hid fee)

--- src/terra/handle_anchor_borrow.py
+++ src/terra/handle_anchor_borrow.py
@@ -2,6 +2,7 @@
 from common.make_tx import make_borrow_tx, make_repay_tx
 from terra import util_terra
 from terra.make_tx import make_deposit_collateral_tx, make_withdraw_collateral_tx
+from terra.constants import MILLION, CUR_UST
 
 
 def handle_deposit_collateral(exporter, elem, txinfo):
@@ -48,10 +49,18 @@ def handle_borrow(exporter, elem, txinfo):
     txid = txinfo.txid
     wallet_address = txinfo.wallet_address
 
-    transfers_in, transfers_out = util_terra._transfers(elem, wallet_address, txid)
-    amount, currency = transfers_in[0]
+    # Extract fee paid by anchor market contract to fee collector
+    fee_collector_address = "terra17xpfvakm2amg962yls6f84z3kell8c5lkaeqfa"
+    transfers_in, _ = util_terra._transfers(elem, fee_collector_address, txid)
+    fee_amount, fee_currency = transfers_in[0]
 
-    row = make_borrow_tx(txinfo, amount, currency)
+    # Extract borrow amount
+    from_contract = elem["logs"][0]["events_by_type"]["from_contract"]
+    borrow_amount = float(from_contract["borrow_amount"][0]) / MILLION
+
+    row = make_borrow_tx(txinfo, borrow_amount, CUR_UST)
+    row.fee += fee_amount
+
     exporter.ingest_row(row)
 
 
