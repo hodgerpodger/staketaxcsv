f2e1d7631bf4deaf76c65c8c56c87e853858ae7a
JumperAvocado <jumperavocado@gmail.com>
2022-02-12 00:21:50 +0100

    Linting

--- src/common/report_util.py
+++ src/common/report_util.py
@@ -4,7 +4,7 @@ import logging
 import os
 
 from common.ExporterTypes import FORMAT_DEFAULT, FORMATS
-from settings_csv import REPORTS_DIR, TICKER_ALGO, TICKER_ATOM, TICKER_LUNA, TICKER_OSMO
+from settings_csv import REPORTS_DIR, TICKER_ATOM, TICKER_LUNA, TICKER_OSMO
 
 ALL = "all"
 
