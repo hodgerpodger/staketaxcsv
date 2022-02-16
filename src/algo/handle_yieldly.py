4a2ffb953c2a438acdb77c880f9e0118f8187196
jumperavocado <98097565+jumperavocado@users.noreply.github.com>
2022-02-17 00:45:27 +0100

    Added support for Yieldly closeout transactions (#66)

--- src/algo/handle_yieldly.py
+++ src/algo/handle_yieldly.py
@@ -25,7 +25,13 @@ APPLICATION_ID_YIELDLY_YLDY_TINY_POOL = 593270704
 APPLICATION_ID_YIELDLY_YLDY_TREES_POOL = 593289960
 APPLICATION_ID_YIELDLY_YLDY_HDL_POOL = 596950925
 APPLICATION_ID_YIELDLY_YLDY_BLOCK_POOL = 593324268
+APPLICATION_ID_YIELDLY_YLDY_RIO_POOL = 604219363
+APPLICATION_ID_YIELDLY_YLDY_AO_POOL = 604373501
+APPLICATION_ID_YIELDLY_YLDY_CHIP_POOL = 604392265
+APPLICATION_ID_YIELDLY_YLDY_FLAMINGO_POOL = 604411076
+APPLICATION_ID_YIELDLY_YLDY_BIRDS_POOL = 604434381
 APPLICATION_ID_YIELDLY_AKITA_LP_POOL = 511593477
+APPLICATION_ID_YIELDLY_AKTA_LP_POOL = 556355279
 APPLICATION_ID_YIELDLY_XET_LP_POOL = 568949192
 APPLICATION_ID_YIELDLY_ARCC_LP_POOL = 583355704
 APPLICATION_ID_YIELDLY_DEFLY_LP_POOL = 591416743
@@ -34,6 +40,11 @@ APPLICATION_ID_YIELDLY_TINY_LP_POOL = 593278929
 APPLICATION_ID_YIELDLY_TREES_LP_POOL = 593294372
 APPLICATION_ID_YIELDLY_HDL_LP_POOL = 596954871
 APPLICATION_ID_YIELDLY_BLOCK_LP_POOL = 593337625
+APPLICATION_ID_YIELDLY_RIO_LP_POOL = 604223245
+APPLICATION_ID_YIELDLY_AO_LP_POOL = 604375580
+APPLICATION_ID_YIELDLY_CHIP_LP_POOL = 604393901
+APPLICATION_ID_YIELDLY_FLAMINGO_LP_POOL = 604412989
+APPLICATION_ID_YIELDLY_BIRDS_LP_POOL = 604437391
 
 YIELDLY_APPLICATIONS = [
     APPLICATION_ID_YIELDLY,
@@ -59,7 +70,13 @@ YIELDLY_APPLICATIONS = [
     APPLICATION_ID_YIELDLY_YLDY_TREES_POOL,
     APPLICATION_ID_YIELDLY_YLDY_HDL_POOL,
     APPLICATION_ID_YIELDLY_YLDY_BLOCK_POOL,
+    APPLICATION_ID_YIELDLY_YLDY_RIO_POOL,
+    APPLICATION_ID_YIELDLY_YLDY_AO_POOL,
+    APPLICATION_ID_YIELDLY_YLDY_CHIP_POOL,
+    APPLICATION_ID_YIELDLY_YLDY_FLAMINGO_POOL,
+    APPLICATION_ID_YIELDLY_YLDY_BIRDS_POOL,
     APPLICATION_ID_YIELDLY_AKITA_LP_POOL,
+    APPLICATION_ID_YIELDLY_AKTA_LP_POOL,
     APPLICATION_ID_YIELDLY_XET_LP_POOL,
     APPLICATION_ID_YIELDLY_ARCC_LP_POOL,
     APPLICATION_ID_YIELDLY_DEFLY_LP_POOL,
@@ -68,15 +85,21 @@ YIELDLY_APPLICATIONS = [
     APPLICATION_ID_YIELDLY_TREES_LP_POOL,
     APPLICATION_ID_YIELDLY_HDL_LP_POOL,
     APPLICATION_ID_YIELDLY_BLOCK_LP_POOL,
+    APPLICATION_ID_YIELDLY_RIO_LP_POOL,
+    APPLICATION_ID_YIELDLY_AO_LP_POOL,
+    APPLICATION_ID_YIELDLY_CHIP_LP_POOL,
+    APPLICATION_ID_YIELDLY_FLAMINGO_LP_POOL,
+    APPLICATION_ID_YIELDLY_BIRDS_LP_POOL,
 ]
 
 YIELDLY_TRANSACTION_POOL_CLAIM = "Q0E="
 YIELDLY_TRANSACTION_POOL_CLOSE = "Q0FX"
+YIELDLY_TRANSACTION_POOL_BAIL = "YmFpbA=="
 
 
 def is_yieldly_transaction(group):
     length = len(group)
-    if length < 3 or length > 6:
+    if length < 2 or length > 6:
         return False
 
     if (group[0]["tx-type"] != "appl" or group[1]["tx-type"] != "appl"):
@@ -96,21 +119,30 @@ def handle_yieldly_transaction(group, exporter, txinfo):
         row = make_reward_tx(txinfo, reward, reward.ticker)
         exporter.ingest_row(row)
 
-    app_id = group[1][co.TRANSACTION_KEY_APP_CALL]["application-id"]
-    if app_id == APPLICATION_ID_YIELDLY_NLL:
-        _handle_yieldly_nll(group, exporter, txinfo)
-    elif app_id == APPLICATION_ID_YIELDLY_YLDY_ALGO_POOL:
-        _handle_yieldly_algo_pool_claim(group, exporter, txinfo)
-    elif app_id in YIELDLY_APPLICATIONS:
+    app_transaction = group[1]
+    app_id = app_transaction[co.TRANSACTION_KEY_APP_CALL]["application-id"]
+    appl_args = app_transaction[co.TRANSACTION_KEY_APP_CALL]["application-args"]
+    if YIELDLY_TRANSACTION_POOL_CLAIM in appl_args:
+        if app_id == APPLICATION_ID_YIELDLY_NLL:
+            _handle_yieldly_nll(group, exporter, txinfo)
+        elif app_id == APPLICATION_ID_YIELDLY_YLDY_ALGO_POOL:
+            _handle_yieldly_algo_pool_claim(group, exporter, txinfo)
+        elif app_id in YIELDLY_APPLICATIONS:
+            _handle_yieldly_asa_pool_claim(group, exporter, txinfo)
+    elif YIELDLY_TRANSACTION_POOL_CLOSE in appl_args:
+        # Claims and legacy closeouts are handled the same way
         _handle_yieldly_asa_pool_claim(group, exporter, txinfo)
+    elif YIELDLY_TRANSACTION_POOL_BAIL in appl_args:
+        app_transaction = group[0]
+        if (app_transaction[co.TRANSACTION_KEY_APP_CALL]["on-completion"] == "closeout"
+                and "inner-txns" in app_transaction
+                and len(app_transaction["inner-txns"]) == 2):
+            _handle_yieldly_asa_pool_close(group, exporter, txinfo)
 
 
 def _handle_yieldly_nll(group, exporter, txinfo):
-    app_transaction = group[1]
-    appl_args = app_transaction[co.TRANSACTION_KEY_APP_CALL]["application-args"]
-    if YIELDLY_TRANSACTION_POOL_CLAIM not in appl_args:
-        return
     init_transaction = group[0]
+    app_transaction = group[1]
     fee_amount = init_transaction["fee"] + app_transaction["fee"]
 
     asset_transaction = group[2]
@@ -129,11 +161,8 @@ def _handle_yieldly_nll(group, exporter, txinfo):
 
 
 def _handle_yieldly_algo_pool_claim(group, exporter, txinfo):
-    app_transaction = group[1]
-    appl_args = app_transaction[co.TRANSACTION_KEY_APP_CALL]["application-args"]
-    if YIELDLY_TRANSACTION_POOL_CLAIM not in appl_args:
-        return
     init_transaction = group[0]
+    app_transaction = group[1]
     fee_amount = init_transaction["fee"] + app_transaction["fee"]
 
     app_transaction = group[2]
@@ -150,6 +179,7 @@ def _handle_yieldly_algo_pool_claim(group, exporter, txinfo):
     fee_transaction = group[5]
     fee_amount = fee_transaction["fee"] + fee_transaction[co.TRANSACTION_KEY_PAYMENT]["amount"]
 
+    # Distribute fee over the two transactions
     fee = Algo(fee_amount / 2)
     txinfo.fee = fee.amount
     txinfo.comment = "Yieldly Staking Pool"
@@ -162,27 +192,42 @@ def _handle_yieldly_algo_pool_claim(group, exporter, txinfo):
 
 
 def _handle_yieldly_asa_pool_claim(group, exporter, txinfo):
-    app_transaction = group[1]
-    appl_args = app_transaction[co.TRANSACTION_KEY_APP_CALL]["application-args"]
-    is_pool_claim = YIELDLY_TRANSACTION_POOL_CLAIM in appl_args
-    is_pool_close = YIELDLY_TRANSACTION_POOL_CLOSE in appl_args
-    if (not is_pool_claim and not is_pool_close):
-        return
-    global_state = app_transaction["global-state-delta"]
-    has_pending_claim = any(item for item in global_state if item["key"] == YIELDLY_TRANSACTION_POOL_CLAIM)
-    if (is_pool_close and not has_pending_claim):
-        return
     init_transaction = group[0]
+    app_transaction = group[1]
     fee_amount = init_transaction["fee"] + app_transaction["fee"]
 
     asset_transaction = group[2]
     transfer_details = asset_transaction[co.TRANSACTION_KEY_ASSET_TRANSFER]
     reward = Asset(transfer_details["asset-id"], transfer_details["amount"])
 
-    fee = Algo(fee_amount)
-    txinfo.fee = fee.amount
-    txinfo.comment = "Yieldly Staking Pool"
+    if not reward.zero():
+        fee = Algo(fee_amount)
+        txinfo.fee = fee.amount
+        appl_args = app_transaction[co.TRANSACTION_KEY_APP_CALL]["application-args"]
+        if YIELDLY_TRANSACTION_POOL_CLOSE in appl_args:
+            txinfo.comment = "Yieldly Staking Pool Close"
+        else:
+            txinfo.comment = "Yieldly Staking Pool"
 
-    row = make_reward_tx(txinfo, reward, reward.ticker)
-    exporter.ingest_row(row)
-    return
+        row = make_reward_tx(txinfo, reward, reward.ticker)
+        exporter.ingest_row(row)
+
+
+def _handle_yieldly_asa_pool_close(group, exporter, txinfo):
+    app_transaction = group[0]
+    app_bail_transaction = group[1]
+    fee_amount = app_bail_transaction["fee"] + app_transaction["fee"]
+
+    # First inner transaction is a deposit withdraw
+    # Second inner transaction is pending rewards claim
+    asset_transaction = app_transaction["inner-txns"][1]
+    transfer_details = asset_transaction[co.TRANSACTION_KEY_ASSET_TRANSFER]
+    reward = Asset(transfer_details["asset-id"], transfer_details["amount"])
+
+    if not reward.zero():
+        fee = Algo(fee_amount)
+        txinfo.fee = fee.amount
+        txinfo.comment = "Yieldly Staking Pool Close"
+
+        row = make_reward_tx(txinfo, reward, reward.ticker)
+        exporter.ingest_row(row)
