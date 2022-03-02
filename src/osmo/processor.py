295eb8da6bb4df26f5a76244feb9dd2d79c6f490
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2022-03-01 23:34:40 -0800

    osmo refactoring

--- src/osmo/processor.py
+++ src/osmo/processor.py
@@ -4,25 +4,13 @@ from datetime import datetime
 from osmo import constants as co
 from osmo import util_osmo
 from osmo.config_osmo import localconfig
-from osmo.handle_general import (
-    handle_failed_tx,
-    handle_simple,
-    handle_simple_outbound,
-    handle_transfer,
-    handle_transfer_ibc,
-)
-from osmo.handle_lp import (
-    handle_lp_deposit,
-    handle_lp_deposit_partial,
-    handle_lp_stake,
-    handle_lp_unstake,
-    handle_lp_withdraw,
-)
-from osmo.handle_staking import handle_staking
-from osmo.handle_swap import handle_swap
-from osmo.handle_unknown import handle_unknown_detect_transfers
-import osmo.handle_superfluid
 from osmo.TxInfoOsmo import MsgInfo, TxInfoOsmo
+import osmo.handle_general
+import osmo.handle_lp
+import osmo.handle_swap
+import osmo.handle_staking
+import osmo.handle_unknown
+import osmo.handle_superfluid
 
 
 def process_txs(wallet_address, elems, exporter):
@@ -35,7 +23,7 @@ def process_tx(wallet_address, elem, exporter):
 
     # Detect failed transaction
     if elem["code"] > 0:
-        handle_failed_tx(exporter, txinfo)
+        osmo.handle_general.handle_failed_tx(exporter, txinfo)
         return txinfo
 
     for msginfo in txinfo.msgs:
@@ -51,53 +39,53 @@ def _handle_message(exporter, txinfo, msginfo):
         # simple transactions, that are typically ignored
         if msg_type in [co.MSG_TYPE_VOTE, co.MSG_TYPE_SET_WITHDRAW_ADDRESS]:
             # 0 transfers
-            handle_simple(exporter, txinfo, msginfo)
+            osmo.handle_general.handle_simple(exporter, txinfo, msginfo)
         elif msg_type in [co.MSG_TYPE_SUBMIT_PROPOSAL, co.MSG_TYPE_DEPOSIT]:
             # 1 outbound transfer
-            handle_simple_outbound(exporter, txinfo, msginfo)
+            osmo.handle_general.handle_simple_outbound(exporter, txinfo, msginfo)
         elif msg_type in [co.MSG_TYPE_UPDATE_CLIENT, co.MSG_TYPE_ACKNOWLEDGMENT]:
             pass
 
         # staking rewards
         elif msg_type in [co.MSG_TYPE_DELEGATE, co.MSG_TYPE_REDELEGATE, co.MSG_TYPE_WITHDRAW_REWARD,
                           co.MSG_TYPE_WITHDRAW_COMMISSION, co.MSG_TYPE_UNDELEGATE]:
-            handle_staking(exporter, txinfo, msginfo)
+            osmo.handle_staking.handle_staking(exporter, txinfo, msginfo)
 
         # transfers
         elif msg_type in [co.MSG_TYPE_IBC_TRANSFER, co.MSG_TYPE_MSGRECVPACKET]:
-            handle_transfer_ibc(exporter, txinfo, msginfo)
+            osmo.handle_general.handle_transfer_ibc(exporter, txinfo, msginfo)
         elif msg_type == co.MSG_TYPE_TIMEOUT:
             # ibc transfer timeout
             pass
         elif msg_type == co.MSG_TYPE_SEND:
-            handle_transfer(exporter, txinfo, msginfo)
+            osmo.handle_general.handle_transfer(exporter, txinfo, msginfo)
 
         # swaps
         elif msg_type == co.MSG_TYPE_SWAP_IN:
-            handle_swap(exporter, txinfo, msginfo)
+            osmo.handle_swap.handle_swap(exporter, txinfo, msginfo)
 
         # lp transactions
         elif msg_type == co.MSG_TYPE_JOIN_POOL:
-            handle_lp_deposit(exporter, txinfo, msginfo)
+            osmo.handle_lp.handle_lp_deposit(exporter, txinfo, msginfo)
         elif msg_type == co.MSG_TYPE_JOIN_SWAP_EXTERN_AMOUNT_IN:
-            handle_lp_deposit_partial(exporter, txinfo, msginfo)
+            osmo.handle_lp.handle_lp_deposit_partial(exporter, txinfo, msginfo)
         elif msg_type == co.MSG_TYPE_EXIT_POOL:
-            handle_lp_withdraw(exporter, txinfo, msginfo)
+            osmo.handle_lp.handle_lp_withdraw(exporter, txinfo, msginfo)
         elif msg_type == co.MSG_TYPE_LOCK_TOKENS:
-            handle_lp_stake(exporter, txinfo, msginfo)
+            osmo.handle_lp.handle_lp_stake(exporter, txinfo, msginfo)
         elif msg_type == co.MSG_TYPE_BEGIN_UNLOCKING:
-            handle_lp_unstake(exporter, txinfo, msginfo)
+            osmo.handle_lp.handle_lp_unstake(exporter, txinfo, msginfo)
 
         # superfluid
         elif msg_type == co.MSG_TYPE_SUPERFLUID_DELEGATE:
             osmo.handle_superfluid.handle_delegate(exporter, txinfo, msginfo)
 
         else:
-            handle_unknown_detect_transfers(exporter, txinfo, msginfo)
+            osmo.handle_unknown.handle_unknown_detect_transfers(exporter, txinfo, msginfo)
     except Exception as e:
         logging.error(
             "Exception when handling txid=%s, exception=%s", txinfo.txid, str(e))
-        handle_unknown_detect_transfers(exporter, txinfo, msginfo)
+        osmo.handle_unknown.handle_unknown_detect_transfers(exporter, txinfo, msginfo)
 
         if localconfig.debug:
             raise(e)
