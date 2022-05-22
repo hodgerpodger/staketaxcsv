2b3c2cb6433ed7f575b1ef8f0d521ac5ec29d252
mccobr <7819938+mccobr@users.noreply.github.com>
2022-05-22 12:01:39 -0400

    add a placeholder for usage payment processing

--- src/dvpn/progress_dvpn.py
+++ src/dvpn/progress_dvpn.py
@@ -1,7 +1,9 @@
 from common.progress import Progress
 from dvpn.config_dvpn import localconfig
 
-SECONDS_PER_PAGE = 2
+LCD_SECONDS_PER_PAGE = 3
+RPC_SECONDS_PER_PAGE = 2
+USAGE_PAYMENTS_SECONDS_PER_PAGE = 2
 
 
 class ProgressDvpn(Progress):
@@ -10,7 +12,10 @@ class ProgressDvpn(Progress):
         super().__init__(localconfig)
 
     def set_lcd_estimate(self, count_pages):
-        self.add_stage("lcd", count_pages, SECONDS_PER_PAGE)
+        self.add_stage("lcd", count_pages, LCD_SECONDS_PER_PAGE)
 
     def set_rpc_estimate(self, count_pages):
-        self.add_stage("rpc", count_pages, SECONDS_PER_PAGE)
+        self.add_stage("rpc", count_pages, RPC_SECONDS_PER_PAGE)
+
+    def set_usage_payment_estimate(self, count_pages):
+        self.add_stage("usage-payment", count_pages, USAGE_PAYMENTS_SECONDS_PER_PAGE)
