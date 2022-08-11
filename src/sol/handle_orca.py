dbe238ceb7f60dbb7e8ba2d55e5ef818481ccf36
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2022-08-11 10:13:53 -0700

    sol: fixed orca swap case with tiny stSOL balance change

--- src/sol/handle_orca.py
+++ src/sol/handle_orca.py
@@ -1,7 +1,10 @@
 from common.make_tx import make_swap_tx
 from sol.handle_simple import handle_unknown_detect_transfers
-from sol.constants import CURRENCY_SOL
+from sol.constants import CURRENCY_SOL, CURRENCY_STSOL
 from sol.util_sol import FEE_THRESHOLD
+
+
+STSOL_FEE_THRESHOLD = 0.0001
 
 
 def handle_orca_swap_v2(exporter, txinfo):
@@ -12,15 +15,24 @@ def handle_orca_swap_v2(exporter, txinfo):
         received_amount, received_currency, _, _ = transfers_in[0]
         row = make_swap_tx(txinfo, sent_amount, sent_currency, received_amount, received_currency)
         exporter.ingest_row(row)
+        return
     elif len(transfers_in) == 2 and len(transfers_out) == 1:
-        # Detect case where if there's extra SOL transfer in with small amount
         amt1, cur1, _, _ = transfers_in[0]
         amt2, cur2, _, _ = transfers_in[1]
 
         if cur1 == CURRENCY_SOL and amt1 < FEE_THRESHOLD:
+            # Detect case where if there's extra SOL transfer in with small amount
             sent_amount, sent_currency, _, _ = transfers_out[0]
             received_amount, received_currency = amt2, cur2
             row = make_swap_tx(txinfo, sent_amount, sent_currency, received_amount, received_currency)
             exporter.ingest_row(row)
-    else:
-        handle_unknown_detect_transfers(exporter, txinfo)
+            return
+        elif cur2 == CURRENCY_STSOL and amt2 < STSOL_FEE_THRESHOLD:
+            # Detect case where if there's extra stSOL transfer in with small amount
+            sent_amount, sent_currency, _, _ = transfers_out[0]
+            received_amount, received_currency = amt1, cur1
+            row = make_swap_tx(txinfo, sent_amount, sent_currency, received_amount, received_currency)
+            exporter.ingest_row(row)
+            return
+
+    handle_unknown_detect_transfers(exporter, txinfo)
