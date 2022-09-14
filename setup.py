25ab38b143badd57b421de538ce5a829835731bb
Yannis Liapis <yanni@yanniss-mbp.lan>
2022-09-14 17:53:33 -0500

    Refactored to hopefully a working fix

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import re
 
 from setuptools import find_packages, setup
 
@@ -5,10 +6,18 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 
+PACKAGE_NAME = "staketaxcsv"
+SOURCE_DIRECTORY = "src"
+
+source_packages = find_packages()
+source_package_regex = re.compile(f"^{SOURCE_DIRECTORY}")
+project_packages = [source_package_regex.sub(PACKAGE_NAME, name) for name in source_packages]
+
+
 setup(
-    name='staketaxcsv',
+    name=PACKAGE_NAME,
     version='0.0.1',
     install_requires=required,
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    package_dir={PACKAGE_NAME: SOURCE_DIRECTORY},
+    packages=project_packages,
 )
