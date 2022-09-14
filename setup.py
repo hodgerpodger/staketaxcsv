58b1cdd2be5eea01054fdd702ae370dee15cf55a
Yannis Liapis <yanni@yanniss-mbp.lan>
2022-09-14 14:21:20 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setup(
+    name='staketaxcsv',
+    version='0.0.1',
+    install_requires=required,
+    packages=find_packages()
+)
