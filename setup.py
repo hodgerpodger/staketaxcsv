d365270d2f6283f16e329ab29a8972169b72ff42
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2022-10-11 12:52:49 -0700

    not close but want to save changes

--- setup.py
+++ setup.py
@@ -7,18 +7,13 @@ with open('requirements.txt') as f:
 
 
 PACKAGE_NAME = "staketaxcsv"
-SOURCE_DIRECTORY = "src"
-
-source_packages = find_packages()
-source_package_regex = re.compile(f"^{SOURCE_DIRECTORY}")
-project_packages = [source_package_regex.sub(PACKAGE_NAME, name) for name in source_packages]
-
+SOURCE_DIRECTORY = "src/staketaxcsv"
 
 setup(
     name=PACKAGE_NAME,
     version='0.0.1',
     install_requires=required,
     package_dir={PACKAGE_NAME: SOURCE_DIRECTORY},
-    packages=project_packages,
+    packages=["staketaxcsv"],
     include_package_data=True,
 )
