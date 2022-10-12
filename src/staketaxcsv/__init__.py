e89d3cf514812956be467bcb84b8a0ccce472b8f
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2022-10-12 14:03:09 -0700

    added help text for staketaxcsv module

--- src/staketaxcsv/__init__.py
+++ src/staketaxcsv/__init__.py
@@ -1,1 +1,31 @@
+"""
+staketaxcsv
+
+Details on usage:
+
+    >>> import staketaxcsv
+    >>> help(staketaxcsv.api)
+
+Example usage:
+
+    >>> import staketaxcsv
+    >>>
+    >>> address = "<SOME_ADDRESS>"
+    >>> txid = "<SOME_TXID>"
+    >>>
+    >>> staketaxcsv.formats()
+    ['default', 'balances', 'accointing', 'bitcointax', 'coinledger', 'coinpanda', 'cointelli', 'cointracking', 'cointracker', 'cryptio', 'cryptocom', 'cryptotaxcalculator', 'cryptoworth', 'koinly', 'recap', 'taxbit', 'tokentax', 'zenledger']
+    >>>
+    >>> staketaxcsv.tickers()
+    ['ALGO', 'ATOM', 'BLD', 'BTSG', 'DVPN', 'EVMOS', 'FET', 'HUAHUA', 'IOTX', 'JUNO', 'KUJI', 'LUNA1', 'LUNA2', 'OSMO', 'REGEN', 'SOL', 'STARS']
+    >>>
+    >>> staketaxcsv.transaction("ATOM", address, txid, "koinly")
+    ...
+    >>> staketaxcsv.csv("ATOM", address, "koinly")
+    ...
+    >>> staketaxcsv.csv_all("ATOM", address)
+    ...
+
+"""
+
 from .api import csv, csv_all, formats, tickers, transaction
