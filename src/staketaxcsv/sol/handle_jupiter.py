c1bf9e61b9b42573773f7f6d4b472939760dcde0
ishiDACo <2542762+ishiDACo@users.noreply.github.com>
2022-10-27 13:41:00 +0900

    sol: fix to correct fee when replacing SOL with Jupeter Aggregator (#210)
    * Sol: Added support for Jupiter Aggregator V3, V4
    
    * sol: fix to correct fee when replacing SOL with Jupeter Aggregator

--- src/staketaxcsv/sol/handle_jupiter.py
+++ src/staketaxcsv/sol/handle_jupiter.py
@@ -1,4 +1,5 @@
 from staketaxcsv.common.make_tx import make_swap_tx
+from staketaxcsv.sol.constants import CURRENCY_SOL
 from staketaxcsv.sol.handle_simple import handle_unknown_detect_transfers
 
 
@@ -23,6 +24,14 @@ def _handle_jupiter_aggregator(exporter, txinfo):
     if len(transfers_in) == 1 and len(transfers_out) == 1:
         sent_amount, sent_currency, _, _ = transfers_out[0]
         received_amount, received_currency, _, _ = transfers_in[0]
+
+        # For SOL transfers, adjust fee from zero to non-zero if applicable
+        if sent_currency == CURRENCY_SOL and txinfo.fee == "" and txinfo.fee_blockchain > 0:
+            txinfo.fee = txinfo.fee_blockchain
+            sent_amount -= txinfo.fee_blockchain
+        if received_currency == CURRENCY_SOL and txinfo.fee == "" and txinfo.fee_blockchain > 0:
+            txinfo.fee = txinfo.fee_blockchain
+
         row = make_swap_tx(txinfo, sent_amount, sent_currency, received_amount, received_currency)
         exporter.ingest_row(row)
     else:
