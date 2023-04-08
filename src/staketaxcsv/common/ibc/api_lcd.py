5a885bf322ee630d40033c216eb67dc1f97bd3b8
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2023-04-07 18:01:33 -0700

    cosmosplus ibc: fix to not crash if node_info lcd endpoint unavailable. choose to fallback to lcd api v1

--- src/staketaxcsv/common/ibc/api_lcd.py
+++ src/staketaxcsv/common/ibc/api_lcd.py
@@ -1,4 +1,8 @@
+import simplejson.errors
+import logging
+
 from staketaxcsv.common.ibc import api_lcd_v1, api_lcd_v2
+
 
 LCD_V2_MIN_VERSION = "0.46.0"
 
@@ -36,7 +40,13 @@ def _node_version(node):
     if node in NodeVersions.versions:
         return NodeVersions.versions[node]
 
-    version = api_lcd_v1.LcdAPI_v1(node).cosmos_sdk_version()
+    try:
+        version = api_lcd_v1.LcdAPI_v1(node).cosmos_sdk_version()
+        logging.info("cosmos sdk version: %s", version)
+    except simplejson.errors.JSONDecodeError as e:
+        logging.error("Unable to determine cosmos sdk version.  Returning dummy version 0.1.1.  "
+                      "Suggest using explicit LcdAPI like api_lcd_v1 or api_lcd_v2 instead.  ")
+        version = "0.1.1"
     NodeVersions.versions[node] = version
     return version
 
