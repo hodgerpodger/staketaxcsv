cd18f033749eb81e3b2047119782cbf74e456a55
JumperAvocado <jumperavocado@gmail.com>
2023-04-14 14:35:12 +0200

    algo: Moved Tinyman v2 to plugins

--- src/staketaxcsv/algo/handle_group.py
+++ src/staketaxcsv/algo/handle_group.py
@@ -17,7 +17,6 @@ from staketaxcsv.algo.handle_gard import handle_gard_transaction, is_gard_transa
 from staketaxcsv.algo.handle_humbleswap import handle_humbleswap_transaction, is_humbleswap_transaction
 from staketaxcsv.algo.handle_pact import handle_pact_transaction, is_pact_transaction
 from staketaxcsv.algo.handle_tinyman import handle_tinyman_transaction, is_tinyman_transaction
-from staketaxcsv.algo.handle_tinymanv2 import handle_tinymanv2_transaction, is_tinymanv2_transaction
 from staketaxcsv.algo.handle_transfer import (
     handle_governance_reward_transaction,
     handle_transfer_transactions,
@@ -78,9 +77,6 @@ def handle_transaction_group(wallet_address, dapps, group, exporter, txinfo):
 
     elif is_tinyman_transaction(group):
         handle_tinyman_transaction(group, exporter, txinfo)
-
-    elif is_tinymanv2_transaction(wallet_address, group):
-        handle_tinymanv2_transaction(wallet_address, group, exporter, txinfo)
 
     elif is_pact_transaction(wallet_address, group):
         handle_pact_transaction(wallet_address, group, exporter, txinfo)
