6fc0878c86196ba5472bcd27c030c06f362b6448
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-01-08 20:56:16 -0800

    rowan: refactored into TxData pattern

--- src/staketaxcsv/report_rowan.py
+++ src/staketaxcsv/report_rowan.py
@@ -16,6 +16,7 @@ from staketaxcsv.settings_csv import ROWAN_NODE, TICKER_ROWAN
 from staketaxcsv.rowan.config_rowan import localconfig
 from staketaxcsv.rowan.progress_rowan import SECONDS_PER_PAGE, ProgressRowan
 from staketaxcsv.common.ibc import api_lcd
+from staketaxcsv.common.ibc.tx_data import TxDataLcd
 
 
 def main():
@@ -28,12 +29,17 @@ def read_options(options):
     logging.info("localconfig: %s", localconfig.__dict__)
 
 
+def _txdata():
+    max_txs = localconfig.limit
+    return TxDataLcd(ROWAN_NODE, max_txs)
+
+
 def wallet_exists(wallet_address):
     return api_lcd.make_lcd_api(ROWAN_NODE).account_exists(wallet_address)
 
 
 def txone(wallet_address, txid):
-    elem = api_lcd.make_lcd_api(ROWAN_NODE).get_tx(txid)
+    elem = _txdata().get_tx(txid)
 
     exporter = Exporter(wallet_address, localconfig, TICKER_ROWAN)
     txinfo = staketaxcsv.rowan.processor.process_tx(wallet_address, elem, exporter)
@@ -42,8 +48,7 @@ def txone(wallet_address, txid):
 
 
 def estimate_duration(wallet_address):
-    max_txs = localconfig.limit
-    return SECONDS_PER_PAGE * api_lcd.get_txs_pages_count(ROWAN_NODE, wallet_address, max_txs)
+    return SECONDS_PER_PAGE * _txdata().get_txs_pages_count(wallet_address)
 
 
 def txhistory(wallet_address):
@@ -51,16 +56,16 @@ def txhistory(wallet_address):
         localconfig.ibc_addresses = Cache().get_ibc_addresses()
         logging.info("Loaded ibc_addresses from cache ...")
 
-    max_txs = localconfig.limit
     progress = ProgressRowan()
     exporter = Exporter(wallet_address, localconfig, TICKER_ROWAN)
+    txdata = _txdata()
 
     # Fetch count of transactions to estimate progress more accurately
-    count_pages = api_lcd.get_txs_pages_count(ROWAN_NODE, wallet_address, max_txs)
+    count_pages = txdata.get_txs_pages_count(wallet_address)
     progress.set_estimate(count_pages)
 
     # Fetch transactions
-    elems = api_lcd.get_txs_all(ROWAN_NODE, wallet_address, max_txs, progress=progress)
+    elems = txdata.get_txs_all(wallet_address, progress)
 
     progress.report_message(f"Processing {len(elems)} transactions... ")
     staketaxcsv.rowan.processor.process_txs(wallet_address, elems, exporter)
