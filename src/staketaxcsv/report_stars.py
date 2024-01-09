0e36a708b28c6ff33112169e87de9a3d9340e628
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-01-08 21:00:00 -0800

    stars: refactor into TxData pattern

--- src/staketaxcsv/report_stars.py
+++ src/staketaxcsv/report_stars.py
@@ -19,6 +19,7 @@ from staketaxcsv.settings_csv import STARS_NODE, TICKER_STARS
 from staketaxcsv.stars.config_stars import localconfig
 from staketaxcsv.stars.progress_stars import SECONDS_PER_PAGE, ProgressStars
 from staketaxcsv.common.ibc import api_lcd
+from staketaxcsv.common.ibc.tx_data import TxDataLcd
 
 
 def main():
@@ -31,12 +32,17 @@ def read_options(options):
     logging.info("localconfig: %s", localconfig.__dict__)
 
 
+def _txdata():
+    max_txs = localconfig.limit
+    return TxDataLcd(STARS_NODE, max_txs)
+
+
 def wallet_exists(wallet_address):
     return api_lcd.make_lcd_api(STARS_NODE).account_exists(wallet_address)
 
 
 def txone(wallet_address, txid):
-    elem = api_lcd.make_lcd_api(STARS_NODE).get_tx(txid)
+    elem = _txdata().get_tx(txid)
 
     exporter = Exporter(wallet_address, localconfig, TICKER_STARS)
     txinfo = staketaxcsv.stars.processor.process_tx(wallet_address, elem, exporter)
@@ -45,9 +51,7 @@ def txone(wallet_address, txid):
 
 
 def estimate_duration(wallet_address):
-    max_txs = localconfig.limit
-    return SECONDS_PER_PAGE * api_lcd.get_txs_pages_count(
-        STARS_NODE, wallet_address, max_txs)
+    return SECONDS_PER_PAGE * _txdata().get_txs_pages_count(wallet_address)
 
 
 def txhistory(wallet_address):
@@ -55,17 +59,16 @@ def txhistory(wallet_address):
         localconfig.ibc_addresses = Cache().get_ibc_addresses()
         logging.info("Loaded ibc_addresses from cache ...")
 
-    max_txs = localconfig.limit
     progress = ProgressStars()
     exporter = Exporter(wallet_address, localconfig, TICKER_STARS)
+    txdata = _txdata()
 
     # Fetch count of transactions to estimate progress beforehand
-    count_pages = api_lcd.get_txs_pages_count(
-        STARS_NODE, wallet_address, max_txs)
+    count_pages = txdata.get_txs_pages_count(wallet_address)
     progress.set_estimate(count_pages)
 
     # Fetch transactions
-    elems = api_lcd.get_txs_all(STARS_NODE, wallet_address, max_txs, progress=progress)
+    elems = txdata.get_txs_all(wallet_address, progress)
 
     progress.report_message(f"Processing {len(elems)} transactions... ")
     staketaxcsv.stars.processor.process_txs(wallet_address, elems, exporter)
