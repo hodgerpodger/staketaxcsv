d77e13a894fabbb0b4f940283117eedd9d72b813
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-01-08 20:44:15 -0800

    kuji: refactored into TxData pattern

--- src/staketaxcsv/report_kuji.py
+++ src/staketaxcsv/report_kuji.py
@@ -15,6 +15,7 @@ from staketaxcsv.common.Exporter import Exporter
 from staketaxcsv.kuji.config_kuji import localconfig
 from staketaxcsv.kuji.progress_kuji import SECONDS_PER_PAGE, ProgressKuji
 from staketaxcsv.settings_csv import KUJI_NODE, TICKER_KUJI
+from staketaxcsv.common.ibc.tx_data import TxDataLcd
 LIMIT_PER_QUERY = 15
 
 
@@ -28,12 +29,17 @@ def read_options(options):
     logging.info("localconfig: %s", localconfig.__dict__)
 
 
+def _txdata():
+    max_txs = localconfig.limit
+    return TxDataLcd(KUJI_NODE, max_txs, limit_per_query=LIMIT_PER_QUERY)
+
+
 def wallet_exists(wallet_address):
     return api_lcd.make_lcd_api(KUJI_NODE).account_exists(wallet_address)
 
 
 def txone(wallet_address, txid):
-    elem = api_lcd.make_lcd_api(KUJI_NODE).get_tx(txid)
+    elem = _txdata().get_tx(txid)
 
     exporter = Exporter(wallet_address, localconfig, TICKER_KUJI)
     txinfo = staketaxcsv.kuji.processor.process_tx(wallet_address, elem, exporter)
@@ -42,9 +48,7 @@ def txone(wallet_address, txid):
 
 
 def estimate_duration(wallet_address):
-    max_txs = localconfig.limit
-    return SECONDS_PER_PAGE * api_lcd.get_txs_pages_count(
-        KUJI_NODE, wallet_address, max_txs, limit=LIMIT_PER_QUERY)
+    return SECONDS_PER_PAGE * _txdata().get_txs_pages_count(wallet_address)
 
 
 def txhistory(wallet_address):
@@ -52,17 +56,16 @@ def txhistory(wallet_address):
         localconfig.ibc_addresses = Cache().get_ibc_addresses()
         logging.info("Loaded ibc_addresses from cache ...")
 
-    max_txs = localconfig.limit
     progress = ProgressKuji()
     exporter = Exporter(wallet_address, localconfig, TICKER_KUJI)
+    txdata = _txdata()
 
     # Fetch count of transactions to estimate progress more accurately
-    count_pages = api_lcd.get_txs_pages_count(
-        KUJI_NODE, wallet_address, max_txs, limit=LIMIT_PER_QUERY)
+    count_pages = txdata.get_txs_pages_count(wallet_address)
     progress.set_estimate(count_pages)
 
     # Fetch transactions
-    elems = api_lcd.get_txs_all(KUJI_NODE, wallet_address, max_txs, progress=progress, limit=LIMIT_PER_QUERY)
+    elems = txdata.get_txs_all(wallet_address, progress)
 
     progress.report_message(f"Processing {len(elems)} transactions... ")
     staketaxcsv.kuji.processor.process_txs(wallet_address, elems, exporter)
