64d2a9b29a6fc2dda6fff8730d847a369caa439a
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-01-08 20:49:07 -0800

    kyve: refactored into TxData pattern

--- src/staketaxcsv/report_kyve.py
+++ src/staketaxcsv/report_kyve.py
@@ -14,6 +14,7 @@ from staketaxcsv.common.Cache import Cache
 from staketaxcsv.common.Exporter import Exporter
 from staketaxcsv.settings_csv import KYVE_NODE, TICKER_KYVE
 from staketaxcsv.common.ibc import api_lcd
+from staketaxcsv.common.ibc.tx_data import TxDataLcd
 
 
 def main():
@@ -25,12 +26,17 @@ def read_options(options):
     logging.info("localconfig: %s", localconfig.__dict__)
 
 
+def _txdata():
+    max_txs = localconfig.limit
+    return TxDataLcd(KYVE_NODE, max_txs)
+
+
 def wallet_exists(wallet_address):
     return api_lcd.make_lcd_api(KYVE_NODE).account_exists(wallet_address)
 
 
 def txone(wallet_address, txid):
-    elem = api_lcd.make_lcd_api(KYVE_NODE).get_tx(txid)
+    elem = _txdata().get_tx(txid)
 
     exporter = Exporter(wallet_address, localconfig, TICKER_KYVE)
     txinfo = staketaxcsv.kyve.processor.process_tx(wallet_address, elem, exporter)
@@ -39,8 +45,7 @@ def txone(wallet_address, txid):
 
 
 def estimate_duration(wallet_address):
-    max_txs = localconfig.limit
-    return SECONDS_PER_PAGE * api_lcd.get_txs_pages_count(KYVE_NODE, wallet_address, max_txs)
+    return SECONDS_PER_PAGE * _txdata().get_txs_pages_count(wallet_address)
 
 
 def txhistory(wallet_address):
@@ -49,16 +54,16 @@ def txhistory(wallet_address):
         localconfig.ibc_addresses = Cache().get_ibc_addresses()
         logging.info("Loaded ibc_addresses from cache ...")
 
-    max_txs = localconfig.limit
     progress = ProgressKYVE()
     exporter = Exporter(wallet_address, localconfig, TICKER_KYVE)
+    txdata = _txdata()
 
     # Fetch count of transactions to estimate progress more accurately
-    count_pages = api_lcd.get_txs_pages_count(KYVE_NODE, wallet_address, max_txs)
+    count_pages = txdata.get_txs_pages_count(wallet_address)
     progress.set_estimate(count_pages)
 
     # Fetch transactions
-    elems = api_lcd.get_txs_all(KYVE_NODE, wallet_address, max_txs, progress=progress)
+    elems = txdata.get_txs_all(wallet_address, progress)
 
     progress.report_message(f"Processing {len(elems)} transactions... ")
     staketaxcsv.kyve.processor.process_txs(wallet_address, elems, exporter)
