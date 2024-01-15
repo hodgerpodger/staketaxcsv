090fd3dc981ecf99312d72c013ec4eabf99025b9
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-01-14 19:18:03 -0800

    osmo: migrate test to use mintscan data instead of osmo api

--- src/tests/utils_osmo.py
+++ src/tests/utils_osmo.py
@@ -9,22 +9,26 @@ from staketaxcsv.settings_csv import TICKER_OSMO
 from staketaxcsv.common.Exporter import Exporter
 import staketaxcsv.osmo.api_data
 import staketaxcsv.osmo.processor
+from tests.mock_mintscan import MockMintscanAPI
+from staketaxcsv.report_osmo import _txdata
 
 
 def run_test(wallet_address, txid):
     return run_test_txids(wallet_address, [txid])
 
 
-@patch("staketaxcsv.osmo.api_data.get_tx", mock_get_tx)
+@patch("staketaxcsv.common.ibc.tx_data.MintscanAPI", new=MockMintscanAPI)
 @patch("staketaxcsv.common.ibc.api_lcd_v1.LcdAPI_v1", new=MockLcdAPI_v1)
 @patch("staketaxcsv.osmo.MsgInfoOsmo.get_symbol", mock_get_symbol)
 @patch("staketaxcsv.osmo.MsgInfoOsmo.get_exponent", mock_get_exponent)
 def run_test_txids(wallet_address, txids):
     exporter = Exporter(wallet_address, localconfig, TICKER_OSMO)
 
+    txdata = _txdata()
+
     elems = []
     for txid in txids:
-        elem = staketaxcsv.osmo.api_data.get_tx(txid)
+        elem = txdata.get_tx(txid)
         elems.append(elem)
 
     staketaxcsv.osmo.processor.process_txs(wallet_address, elems, exporter)
