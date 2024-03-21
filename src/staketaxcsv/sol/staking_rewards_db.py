bf83a1f0e610ac03455927a10f17adc227174aa2
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-03-21 14:29:59 -0700

    sol: comments and logging

--- src/staketaxcsv/sol/staking_rewards_db.py
+++ src/staketaxcsv/sol/staking_rewards_db.py
@@ -53,6 +53,7 @@ def rewards_all_users_write_db():
             # Retrieve rewards for all users in this epoch
             logging.info("Retrieving block rewards for epoch=%s ...", epoch)
             block_rewards = RpcAPI.get_block_rewards(slot)
+            logging.info("Found len(block_rewards)=%s", len(block_rewards))
             time.sleep(30)  # throttled at times if too frequent
 
             list_block_rewards.append((epoch, slot, block_rewards))
