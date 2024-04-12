3c3dcc3d777a1d018e19548c8df31e2cf4ed45fd
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-04-11 18:09:09 -0700

    osmo: added fix for concentrated-lp-add-to-position to handle extra incentive reward row if exists

--- src/staketaxcsv/osmo/handle_concentrated_lp.py
+++ src/staketaxcsv/osmo/handle_concentrated_lp.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 from staketaxcsv.osmo import util_osmo
 from staketaxcsv.osmo.make_tx import (
     make_osmo_lp_deposit_tx, make_osmo_lp_withdraw_tx, make_osmo_reward_tx)
+from staketaxcsv.settings_csv import OSMO_NODE
+from staketaxcsv.common.ibc import denoms
 
 
 class PositionLiquidity:
@@ -63,6 +65,23 @@ def handle_create_position(exporter, txinfo, msginfo):
         ]
         util_osmo._ingest_rows(exporter, rows, comment)
         return
+    elif len(transfers_out) == 1:
+        create_position = events_by_type["create_position"]
+        pool_id = create_position["pool_id"]
+        position_id = create_position["position_id"]
+        liquidity = float(create_position["liquidity"])
+
+        lp_amount = liquidity
+        lp_currency = _lp_currency(pool_id)
+        sent_amount1, sent_currency1 = transfers_out[0]
+
+        PositionLiquidity.create_position(position_id, liquidity)
+        comment = f"concentrated_lp.create_position [pool_id={pool_id}][position_id={position_id}] "
+        rows = [
+            make_osmo_lp_deposit_tx(txinfo, msginfo, sent_amount1, sent_currency1, lp_amount, lp_currency),
+        ]
+        util_osmo._ingest_rows(exporter, rows, comment)
+        return
 
     raise Exception("Unable to handle tx in handle_create_position()")
 
@@ -71,7 +90,7 @@ def handle_add_to_position(exporter, txinfo, msginfo):
     transfers_in, transfers_out = msginfo.transfers_net
     events_by_type = msginfo.events_by_type
 
-    if len(transfers_in) == 0 and len(transfers_out) == 2:
+    if len(transfers_in) <= 1 and len(transfers_out) == 2:
         withdraw_position_id = events_by_type["withdraw_position"]["position_id"]
         withdraw_pool_id = events_by_type["withdraw_position"]["pool_id"]
 
@@ -91,12 +110,23 @@ def handle_add_to_position(exporter, txinfo, msginfo):
 
         comment = f"concentrated_lp.add_to_position [pool_id={create_pool_id}]" + \
                   f"[old position_id={withdraw_position_id}][new position_id={create_position_id}] "
+
         rows = [
             make_osmo_lp_deposit_tx(txinfo, msginfo, sent_amount1, sent_currency1, lp_amount / 2, lp_currency),
             make_osmo_lp_deposit_tx(txinfo, msginfo, sent_amount2, sent_currency2, lp_amount / 2, lp_currency),
         ]
         util_osmo._ingest_rows(exporter, rows, comment)
+
+        # Handle extra reward if exists
+        if len(transfers_in) == 1:
+            tokens_out = events_by_type.get("collect_incentives", {}).get("tokens_out", None)
+            # i.e. 1268498uosmo
+            amt, cur = msginfo.amount_currency(tokens_out)[0]
+            row = make_osmo_reward_tx(txinfo, msginfo, amt, cur)
+            util_osmo._ingest_rows(exporter, [row], comment)
+
         return
+
 
     raise Exception("Unable to handle tx in handle_add_to_position()")
 
@@ -161,5 +191,29 @@ def handle_withdraw_position(exporter, txinfo, msginfo):
         ]
         util_osmo._ingest_rows(exporter, rows, comment)
         return
+
+    if len(transfers_in) == 3 and len(transfers_out) == 0:
+        position_id = events_by_type["withdraw_position"]["position_id"]
+        liquidity = abs(float(events_by_type["withdraw_position"]["liquidity"]))
+        pool_id = events_by_type["withdraw_position"]["pool_id"]
+
+        lp_amount = liquidity
+        lp_currency = _lp_currency(pool_id)
+
+        receive_amount1, receive_currency1 = transfers_in[0]
+        receive_amount2, receive_currency2 = transfers_in[1]
+        receive_amount3, receive_currency3 = transfers_in[2]
+
+        PositionLiquidity.withdraw_position(position_id, liquidity)
+
+        comment = f"concentrated_lp.withdraw_position [pool_id={pool_id}][position_id={position_id}] "
+        rows = [
+            make_osmo_lp_withdraw_tx(txinfo, msginfo, lp_amount / 3, lp_currency, receive_amount1, receive_currency1),
+            make_osmo_lp_withdraw_tx(txinfo, msginfo, lp_amount / 3, lp_currency, receive_amount2, receive_currency2),
+            make_osmo_lp_withdraw_tx(txinfo, msginfo, lp_amount / 3, lp_currency, receive_amount3, receive_currency3),
+        ]
+        util_osmo._ingest_rows(exporter, rows, comment)
+        return
+
 
     raise Exception("Unable to handle tx in handle_withdraw_position()")
