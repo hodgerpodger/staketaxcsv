22fde9dfb1e737892ee27b9642dd5a2fdd0184ff
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-08-15 15:14:49 -0700

    osmo: mars_red_bank - redid deposit case and added withdraw case

--- src/staketaxcsv/osmo/contracts/mars_red_bank.py
+++ src/staketaxcsv/osmo/contracts/mars_red_bank.py
@@ -1,5 +1,5 @@
 from staketaxcsv.common.make_tx import make_borrow_tx, make_repay_tx
-from staketaxcsv.common.make_tx import make_spend_fee_tx
+from staketaxcsv.osmo.make_tx import make_osmo_transfer_out_tx, make_osmo_transfer_in_tx
 
 
 def handle_red_bank(exporter, txinfo, msginfo):
@@ -7,6 +7,8 @@ def handle_red_bank(exporter, txinfo, msginfo):
 
     if "deposit" in msg:
         return _handle_deposit(exporter, txinfo, msginfo)
+    if "withdraw" in msg:
+        return _handle_withdraw(exporter, txinfo, msginfo)
     if "borrow" in msg:
         return _handle_borrow(exporter, txinfo, msginfo)
     if "repay" in msg:
@@ -21,8 +23,22 @@ def _handle_deposit(exporter, txinfo, msginfo):
     if len(transfers_out) == 1 and len(transfers_in) == 0:
         sent_amt, sent_cur = transfers_out[0]
 
-        row = make_spend_fee_tx(txinfo, txinfo.fee, txinfo.fee_currency)
+        row = make_osmo_transfer_out_tx(txinfo, msginfo, sent_amt, sent_cur)
         row.comment += f"[mars_red_bank deposit {sent_amt} {sent_cur}]"
+        exporter.ingest_row(row)
+        return
+
+    raise Exception("Unable to handle tx in mars_red_bank._handle_deposit()")
+
+
+def _handle_withdraw(exporter, txinfo, msginfo):
+    transfers_in, transfers_out = msginfo.transfers
+
+    if len(transfers_in) == 1 and len(transfers_out) == 0:
+        rec_amt, rec_cur = transfers_in[0]
+
+        row = make_osmo_transfer_in_tx(txinfo, msginfo, rec_amt, rec_cur)
+        row.comment += f"[mars_red_bank withdraw {rec_amt} {rec_cur}]"
         exporter.ingest_row(row)
         return
 
