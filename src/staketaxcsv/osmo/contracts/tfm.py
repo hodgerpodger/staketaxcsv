787faff08649c1a9f1489da3732b23ae2e7b97df
hodgerpodger <2503441+hodgerpodger@users.noreply.github.com>
2024-10-14 13:28:21 -0700

    osmo: added tfm cancel limit order

--- src/staketaxcsv/osmo/contracts/tfm.py
+++ src/staketaxcsv/osmo/contracts/tfm.py
@@ -1,5 +1,5 @@
 from staketaxcsv.osmo.make_tx import make_osmo_swap_tx, make_osmo_tx
-from staketaxcsv.common.ExporterTypes import TX_TYPE_TFM_LIMIT_ORDER_SUBMIT
+from staketaxcsv.common.ExporterTypes import TX_TYPE_TFM_SUBMIT_LIMIT_ORDER, TX_TYPE_TFM_CANCEL_LIMIT_ORDER
 
 
 class LimitOrder:
@@ -30,6 +30,17 @@ class LimitOrder:
         comment_text = f"[tfm limit order, execute, order_id={order_id}, received {receive_amount} {receive_currency}"
         return sent_amount, sent_currency, receive_amount, receive_currency, comment_text
 
+    def cancel(self, msginfo):
+        transfers_in, transfers_out = msginfo.transfers
+        assert len(transfers_in) == 1
+        assert len(transfers_out) == 0
+        receive_amount, receive_currency = transfers_in[0]
+
+        order_id = msginfo.events_by_type["wasm"]["order_id"]
+        comment_text = f"[tfm limit order, cancel, order_id={order_id}, received back {receive_amount} {receive_currency}]"
+
+        return comment_text
+
 
 def handle_execute_swap_operations(exporter, txinfo, msginfo):
     transfers_in, transfers_out = msginfo.transfers
@@ -53,6 +64,8 @@ def handle_limit_order(exporter, txinfo, msginfo):
         return _handle_submit_order(exporter, txinfo, msginfo)
     elif len(transfers_in) == 1 and len(transfers_out) == 0 and "execute_order" in execute_contract_message:
         return _handle_execute_order(exporter, txinfo, msginfo)
+    elif len(transfers_in) == 1 and len(transfers_out) == 0 and "cancel_order" in execute_contract_message:
+        return _handle_cancel_order(exporter, txinfo, msginfo)
 
     raise Exception("Unable to handle tx in tfm.handle_limit_order()")
 
@@ -60,7 +73,7 @@ def handle_limit_order(exporter, txinfo, msginfo):
 def _handle_submit_order(exporter, txinfo, msginfo):
     comment_text = LimitOrder().submit(msginfo)
 
-    row = make_osmo_tx(txinfo, msginfo, "", "", "", "", tx_type=TX_TYPE_TFM_LIMIT_ORDER_SUBMIT)
+    row = make_osmo_tx(txinfo, msginfo, "", "", "", "", tx_type=TX_TYPE_TFM_SUBMIT_LIMIT_ORDER)
     row.comment += comment_text
     exporter.ingest_row(row)
 
@@ -69,5 +82,13 @@ def _handle_execute_order(exporter, txinfo, msginfo):
     sent_amount, sent_cur, rec_amount, rec_cur, comment_text = LimitOrder().execute(msginfo)
 
     row = make_osmo_swap_tx(txinfo, msginfo, sent_amount, sent_cur, rec_amount, rec_cur)
+    row.comment += comment_text
+    exporter.ingest_row(row)
+
+
+def _handle_cancel_order(exporter, txinfo, msginfo):
+    comment_text = LimitOrder().cancel(msginfo)
+
+    row = make_osmo_tx(txinfo, msginfo, "", "", "", "", tx_type=TX_TYPE_TFM_CANCEL_LIMIT_ORDER)
     row.comment += comment_text
     exporter.ingest_row(row)
--- src/tests/tests/test_osmo_tfm.py
+++ src/tests/tests/test_osmo_tfm.py
@@ -26,3 +26,16 @@ timestamp            tx_type  received_amount     received_currency  sent_amount
 -------------------  -------  ------------------  -----------------  -----------  -------------  --------  ------------  ------------------------------------------------------------------
         """
         self.assertEqual(result, correct_result.strip(), result)
+
+    def test_cancel_limit_order(self):
+        result = run_test(
+            "osmo10a70lpas3rkydmpnqs0dtr5my5kt8ggf4lqwjh",
+            "4AFF2D0D6FE143DE75A9A5A4E0913ECB0A0BE1857D6BA6038DB39C637DE09267"
+        )
+        correct_result = """
+-------------------  -----------------------  ---------------  -----------------  -----------  -------------  -----------  ------------  ------------------------------------------------------------------
+timestamp            tx_type                  received_amount  received_currency  sent_amount  sent_currency  fee          fee_currency  txid
+2024-02-13 16:43:03  _TFM_CANCEL_LIMIT_ORDER                                                                  0.000572000  OSMO          4AFF2D0D6FE143DE75A9A5A4E0913ECB0A0BE1857D6BA6038DB39C637DE09267-0
+-------------------  -----------------------  ---------------  -----------------  -----------  -------------  -----------  ------------  ------------------------------------------------------------------
+        """
+        self.assertEqual(result, correct_result.strip(), result)
