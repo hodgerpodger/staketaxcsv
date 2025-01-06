e6bb0798b1e2c78aafafaa15873002381992d4a0
Turadg Aleahmad <turadg@agoric.com>
2025-01-06 09:43:16 -0800

    fix: BLD transaction link
    https://agoric.bigdipper.live/ is out of service. Link to Mintscan instead.

--- src/staketaxcsv/bld/processor.py
+++ src/staketaxcsv/bld/processor.py
@@ -14,7 +14,7 @@ def process_txs(wallet_address, elems, exporter):
 
 def process_tx(wallet_address, elem, exporter):
     txinfo = staketaxcsv.common.ibc.processor.txinfo(wallet_address, elem, co.MINTSCAN_LABEL_BLD, BLD_NODE)
-    txinfo.url = "https://agoric.bigdipper.live/transactions/{}".format(txinfo.txid)
+    txinfo.url = "https://www.mintscan.io/agoric/tx/{}".format(txinfo.txid)
 
     if txinfo.is_failed:
         staketaxcsv.common.ibc.processor.handle_failed_transaction(exporter, txinfo)
